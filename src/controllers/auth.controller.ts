9393556b45da0f183a86d021e59f5abc53b32dc9
Lucas Mourão <lucasemourao2@gmail.com>
2026-02-07 08:56:05 -0300

    feat: melhorias no método de login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -9,25 +9,35 @@ export const register = async (request: FastifyRequest, reply: FastifyReply) => 
 
     const user = await registerUser(validation);
 
-    const token = request.server.jwt.sign({ userId: user.id })
+    const token = request.server.jwt.sign({ userId: user.id });
 
     reply.status(201).send({
         user,
         token
-    })
-}
+    });
+};
 
 export const login = async (request: FastifyRequest<{ Body: AuthRequest }>, reply: FastifyReply) => {
 
     const validation = loginSchema.parse(request.body as AuthRequest);
 
-    const user = await loginUser(validation);
+    const user = await loginUser(validation, reply);
+
+    if(!user) return;
 
     const token = request.server.jwt.sign({ userId: user.id });
+
+    reply.setCookie("syntaxwear.token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24,
+    });
+
     reply.status(200).send({
-        user,
-        token
-    })
-}
+        user
+    });
+};
 
 export const profile = async (request: FastifyRequest, reply: FastifyReply) => reply.send(request.user)
